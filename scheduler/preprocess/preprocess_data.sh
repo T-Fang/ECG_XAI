e674c8c341f192c44c2d86033189503dedba8e99
T-Fang <tianfangsg@gmail.com>
2023-03-14 02:05:34 +0800

    Add more RAM during preprocessing

--- scheduler/preprocess/preprocess_data.sh
+++ scheduler/preprocess/preprocess_data.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 ncpus='24'
-mem='40G'
+mem='60G'
 walltime='30:00:00'
 name='preprocess_data'
 script_location='/home/ftian/storage/ECG_XAI/src/scripts/preprocess/preprocess_data.py'
+activate='/home/ftian/miniconda3/bin/activate'
+deactivate='/home/ftian/miniconda3/bin/deactivate'
 
 function submit_job {
-    cmd_script="source deactivate; source activate ecg_xai; python ${script_location}; source deactivate"
+    cmd_script="source ${activate} ecg_xai; python ${script_location}; source ${deactivate}"
 
     job_path="/home/ftian/storage/ECG_XAI/logs/scheduler/preprocess/${name}/"
     mkdir -p ${job_path}
