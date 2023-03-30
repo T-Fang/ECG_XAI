45e7ac1d83948162ac9f8f132fc31a75a8cf3d30
T-Fang <tianfangsg@gmail.com>
2023-03-30 20:08:22 +0800

    Add function to do scatter plot of agg

--- src/utils/test_utils.py
+++ src/utils/test_utils.py
@@ -1,30 +1,30 @@
 import os
 from matplotlib import pyplot as plt
 import pandas as pd
-import torch
 
 
-def compare_agg_via_scatter(path_to_mid_output: str, x_name, y_name):
+def scatter_two_agg(path_to_agg: str, x_name, y_name):
     """
         Compare the aggregated mid output via scatter plot
         """
-    all_mid_output: pd.DataFrame = pd.read_csv('file.csv', header=0, index_col=0)
-    fig_path = os.path.join(os.path.splitext(path_to_mid_output)[0], 'png')
-    # fig, ax = plt.subplots()
-    # for xlabel, ylabel in self.compared_agg:
-    #     x = self.mid_output_agg[xlabel]
-    #     y = self.mid_output_agg[ylabel]
-    #     corr = torch.corrcoef(torch.stack([x, y], dim=0))[0, 1]
-    #     ax.set_title(f'{ylabel} vs {xlabel} (r={corr:.4f})')
-    #     ax.set_xlabel(xlabel)
-    #     ax.set_ylabel(ylabel)
-    #     ax.scatter(x, y)
-    #     fig.savefig(fig_path)
-    #     ax.clear()
-    # plt.close(fig)
+    all_mid_output: pd.DataFrame = pd.read_csv(path_to_agg, header=0, index_col=0)
+    fig_path = os.path.splitext(path_to_agg)[0] + '.png'
+    # extract column x_name and y_name
+    x = all_mid_output.loc[:, x_name]
+    y = all_mid_output.loc[:, y_name]
+    fig, ax = plt.subplots()
+    corr = x.corr(y)
+    ax.set_title(f'{y_name} vs {x_name} (r={corr:.4f})')
+    ax.set_xlabel(x_name)
+    ax.set_ylabel(y_name)
+    ax.scatter(x, y)
+    plt.show(fig)
+    fig.savefig(fig_path)
+    ax.clear()
+    plt.close(fig)
 
 
-def generate_report(path_to_mid_output, patient_idx: int = 0):
-    all_mid_output: pd.DataFrame = pd.read_csv('file.csv', header=0, index_col=0)
+def generate_report(path_to_agg, patient_idx: int = 0):
+    all_mid_output: pd.DataFrame = pd.read_csv(path_to_agg, header=0, index_col=0)
     mid_output: pd.Series = all_mid_output.loc[patient_idx]
     return mid_output
